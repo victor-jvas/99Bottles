7baf467f73effccb6270519ba6c36bcb402e8691
Victor <victor.jvas1@gmail.com>
2026-03-14 20:56:17 -0300

    Refactor: Removed Duplication.

--- Bottles/Bottles.cs
+++ Bottles/Bottles.cs
@@ -2,30 +2,73 @@
 
 public class Bottles
 {
-    private string verse = "99 bottles of beer on the wall, " +
-                           "99 bottles of beer.\n" +
-                           "Take one down and pass it around, " +
-                           "98 bottles of beer on the wall.\n";
+    public string Song()
+    {
+        return Verses(99, 0);
+    }
     
-
+    public string Verses(int upperVerse, int lowerVerse)
+    {
+        List<string> result = [];
+        for (int i = upperVerse; i >= lowerVerse; i--)
+        {
+            result.Add(Verse(i));
+        }
+       
+        return string.Join("\n", result);        
+    }
+    
     public string Verse(int number)
     {
-        bool hasBeerLeft = number != 0;
-        bool hasMoreThanOneBeer = hasBeerLeft && number != 1;
-        
-        string line1 = hasMoreThanOneBeer ? $"{number} bottles of beer on the wall, " : hasBeerLeft ? $"{number} bottle of beer on the wall, " : "No more bottles of beer on the wall, ";
-        string line2 = hasMoreThanOneBeer ? $"{number} bottles of beer.\n" : hasBeerLeft ? $"{number} bottle of beer.\n" : "no more bottles of beer.\n";
-        
-        string line3 = hasMoreThanOneBeer ? "Take one down and pass it around, " : hasBeerLeft ? "Take it down and pass it around, " : "Go to the store and buy some more, ";
-        
-        hasBeerLeft = --number > 0;
-        hasMoreThanOneBeer = hasBeerLeft && number > 1;
-        
-        string line4 = hasMoreThanOneBeer ? $"{number} bottles of beer on the wall.\n" : hasBeerLeft ? $"{number} bottle of beer on the wall.\n" : "no more bottles of beer on the wall.\n";
-
-        string result = line1 + line2 + line3 + line4;
-                         
-        
-        return result;
+        return $"{Quantity(number).Capitalize()} {Container(number)} of beer on the wall, " +
+               $"{Quantity(number)} {Container(number)} of beer.\n" +
+               Action(number) +
+               $"{Quantity(Successor(number))} {Container(Successor(number))} of beer on the wall.\n";
     }
+
+    private string Container(int number)
+    {
+        if (number == 1)
+        {
+            return "bottle";
+        }
+        return "bottles";
+    }
+
+    private string Pronoun(int number)
+    {
+        if (number == 1)
+        {
+            return "it";
+        }
+        return "one";
+    }
+
+    private string Quantity(int number)
+    {
+        if (number == 0)
+        {
+            return "no more";
+        }
+        return number.ToString();
+    }
+
+    private string Action(int number)
+    {
+        if (number == 0)
+        {
+            return "Go to the store and buy some more, ";
+        }
+        return $"Take {Pronoun(number)} down and pass it around, ";
+    }
+
+    private int Successor(int number)
+    {
+        if (number == 0)
+        {
+            return 99;
+        }
+        return number-1;
+    }
+    
 }
---
+++ Bottles/Utils.cs
@@ -0,0 +1,9 @@
+﻿namespace BottlesOfBeer;
+
+public static class Utils
+{
+    public static string Capitalize(this string input)
+    {
+        return char.ToUpper(input[0]) + input[1..];
+    }
+}
